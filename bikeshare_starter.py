c50c68ecd787449f709fd51a446d2107c46af837
Meister <holger.meister@cariad.technology>
2026-04-07 17:20:26 +0200

    Update user input with validation in get_filters

--- bikeshare_starter.py
+++ bikeshare_starter.py
@@ -5,6 +5,15 @@ import numpy as np
 CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
+VALID_MONTHS = [
+    "january", "february", "march", "april", "may", "june",
+    "july", "august", "september", "october", "november", "december"
+]
+
+VALID_DAYS = [
+    "monday", "tuesday", "wednesday", "thursday", 
+    "friday", "saturday", "sunday"
+]
 
 def get_filters():
     """
@@ -16,14 +25,26 @@ def get_filters():
         (str) day - name of the day of week to filter by, or "all" to apply no day filter
     """
     print('Hello! Let\'s explore some US bikeshare data!')
-    # get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
-
-
+    
+    # get user input for city (chicago, new york city, washington).
+    while True:
+        city = input("Please input City (Chicago, New York City or Washington): ").strip().lower()
+        if city in CITY_DATA:
+            break
+        print("Invalid city! Please choose Chicago, New York City or Washington.")
     # get user input for month (all, january, february, ... , june)
-
-
+    while True:
+        month = input("Please input month (all, january, february, ...): ").strip().lower()
+        if month == 'all' or month in VALID_MONTHS:
+            break
+        print("Invalid input! Please type 'all' or a valid month.")
     # get user input for day of week (all, monday, tuesday, ... sunday)
-
+    while True:
+        day = input("Please input day (all, monday, tuesday, ...): ").strip().lower()
+        if day == 'all' or day in VALID_DAYS:
+            break
+        print("Invalid input! Please type 'all' or a valid day.")
+    
 
     print('-'*40)
     return city, month, day
