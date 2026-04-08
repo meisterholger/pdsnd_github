40eced03b12d1bbc4c3f04b1fcf01d303d5c2dae
Meister <holger.meister@cariad.technology>
2026-04-08 10:00:52 +0200

    Update main for missing CSV

--- bikeshare_starter.py
+++ bikeshare_starter.py
@@ -161,11 +161,11 @@ def main():
     while True:
         city, month, day = get_filters()
         df = load_data(city, month, day)
-
-        time_stats(df)
-        station_stats(df)
-        trip_duration_stats(df)
-        user_stats(df)
+        if df is not None and not df.empty:
+            time_stats(df)
+            station_stats(df)
+            trip_duration_stats(df)
+            user_stats(df)
 
         restart = input('\nWould you like to restart? Enter yes or no.\n')
         if restart.lower() != 'yes':
